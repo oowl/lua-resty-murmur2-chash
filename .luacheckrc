43e3b6b26f91ef983719e29e00682bee43306ca0
owl <ouyangjun1999@gmail.com>
2023-06-24 22:07:33 +0800

    feat: support dynamic crud node
    Signed-off-by: owl <ouyangjun1999@gmail.com>

--- .luacheckrc
+++ .luacheckrc
@@ -4,12 +4,6 @@ redefined       = false
 max_line_length = false
 
 
-not_globals = {
-    "string.len",
-    "table.getn",
-}
-
-
 ignore = {
     "6.", -- ignore whitespace warnings
 }
--- lib/resty/murmur2_chash.lua
+++ lib/resty/murmur2_chash.lua
@@ -1,6 +1,7 @@
 local ffi = require "ffi"
 local ffi_cast = ffi.cast
 local C = ffi.C
+local table = table
 
 local ok, new_tab = pcall(require, "table.new")
 if not ok or type(new_tab) ~= "function" then
@@ -22,6 +23,14 @@ local function calc_hash(value)
     return tonumber(C.ngx_murmur_hash2(ffi_cast('uint8_t *', value), #value))
 end
 
+local function table_length(t)
+    local len=0
+    for k, v in pairs(t) do
+      len=len+1
+    end
+    return len
+end
+
 function _M.new(nodes, args)
     if table.getn(nodes) == 0 then
         return nil, "empty nodes"
@@ -37,9 +46,11 @@ function _M.new(nodes, args)
 
     local chash_arrays = new_tab(length, 0)
     local chash_maps = new_tab(0, length)
+    local node_maps = new_tab(0, table.getn(nodes))
 
     for i = 1, table.getn(nodes) do
         local node = nodes[i]
+        node_maps[node] = true
         for j = 1, virtual_node_num do
             local hash = calc_hash(node .. rule .. j)
             chash_maps[hash] = node
@@ -50,10 +61,11 @@ function _M.new(nodes, args)
     table.sort(chash_arrays)
 
     local self = {
+        virtual_node_num = virtual_node_num,
         chash_arrays = chash_arrays,
         chash_maps = chash_maps,
         length = length,
-        nodes = nodes,
+        nodes = node_maps,
         rule = rule,
     }
     return setmetatable(self, mt)
@@ -86,4 +98,69 @@ function _M:get_node(key)
     local hash = calc_hash(key)
     local chash_value = find_chash_value(self.chash_arrays, self.length, hash)
     return self.chash_maps[chash_value]
+end
+
+function _M:add_node(node)
+    if self.nodes[node] then
+        return nil, "node already exists"
+    end
+
+    self.nodes[node] = true
+
+    local virtual_node_num = self.virtual_node_num
+    local rule = self.rule
+    local length = table_length(self.nodes) * virtual_node_num
+
+    local chash_arrays = new_tab(length, 0)
+    local chash_maps = new_tab(0, length)
+
+    for i = 1, table.getn(self.nodes) do
+        local node = self.nodes[i]
+        for j = 1, virtual_node_num do
+            local hash = calc_hash(node .. rule .. j)
+            chash_maps[hash] = node
+            table.insert(chash_arrays, hash)
+        end
+    end
+
+    table.sort(chash_arrays)
+
+    self.chash_arrays = chash_arrays
+    self.chash_maps = chash_maps
+    self.length = length
+
+    return true
+end
+
+
+function _M:delete_node(node)
+    if not self.nodes[node] then
+        return nil, "node not exists"
+    end
+
+    self.nodes[node] = nil
+
+    local virtual_node_num = self.virtual_node_num
+    local rule = self.rule
+    local length = table_length(self.nodes) * virtual_node_num
+
+    local chash_arrays = new_tab(length, 0)
+    local chash_maps = new_tab(0, length)
+
+    for i = 1, table_length(self.nodes) do
+        local node = self.nodes[i]
+        for j = 1, virtual_node_num do
+            local hash = calc_hash(node .. rule .. j)
+            chash_maps[hash] = node
+            table.insert(chash_arrays, hash)
+        end
+    end
+
+    table.sort(chash_arrays)
+
+    self.chash_arrays = chash_arrays
+    self.chash_maps = chash_maps
+    self.length = length
+
+    return true
 end
